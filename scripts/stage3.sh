019bd7f95a95275a6956cd780ea1ea394e6f61ec
arsepav <54571158+arsepav@users.noreply.github.com>
2025-05-08 14:58:57 +0300

    Update stage3.sh

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -2,13 +2,13 @@
 
 export HADOOP_CONF_DIR=/etc/hadoop/conf
 
-spark-submit --master yarn preprocessing.py
+spark-submit --master yarn scripts/preprocessing.py
 
 hdfs dfs -getmerge project/data/train data/train.json
 
 hdfs dfs -getmerge project/data/test data/test.json
 
-spark-submit --master yarn models.py
+spark-submit --master yarn scripts/models.py
 
 hdfs dfs -get -project/output/evaluation /home/team27/output/evaluation.csv
 
